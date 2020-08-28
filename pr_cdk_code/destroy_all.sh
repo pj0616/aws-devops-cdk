b76f737dc9d8bc8e8c4abe96d59b6fd9513c66ab
pat.ryan <pat.ryan@spr.com>
2020-08-27 21:48:04 -0500

    added force to destroy script

--- pr_cdk_code/destroy_all.sh
+++ pr_cdk_code/destroy_all.sh
@@ -1,29 +1,29 @@
 echo 'lambda-stack'
-cdk destroy lambda-stack --profile spr
+cdk destroy lambda-stack --profile spr --force
 
 echo 'apigw-stack'
-cdk destroy apigw-stack --profile spr
+cdk destroy apigw-stack --profile spr --force
 
 echo 'cognito-stack'
-cdk destroy cognito-stack --profile spr
+cdk destroy cognito-stack --profile spr --force
 
 echo 'redis-stack'
-cdk destroy redis-stack --profile spr
+cdk destroy redis-stack --profile spr --force
 
 echo 'rds-stack'
-cdk destroy rds-stack --profile spr
+cdk destroy rds-stack --profile spr --force
 
 echo 's3-stack'
-cdk destroy s3-stack --profile spr
+cdk destroy s3-stack --profile spr --force
 
 echo 'kms-stack'
-cdk destroy kms-stack --profile spr
+cdk destroy kms-stack --profile spr --force
 
 echo 'bastion-stack'
-cdk destroy bastion-stack --profile spr
+cdk destroy bastion-stack --profile spr --force
 
 echo 'security-stack'
-cdk destroy security-stack --profile spr
+cdk destroy security-stack --profile spr --force
 
 echo "vpc-stack"
-cdk destroy vpc-stack --profile spr
+cdk destroy vpc-stack --profile spr --force
